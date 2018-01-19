d66c96f8c99e47ae1adb7e73b12d5a1d484a0b90
Ko-Ching Wu <wukochin@Ko-Chings-MBP.san.rr.com>
2018-01-18 18:17:21 -0800

    Handle squirrel events

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+
+const setupEvents = require('./installers/setupEvents')
+if ( setupEvents.handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
 const electron = require( 'electron' )
 // Module to control application life.
 const app = electron.app
